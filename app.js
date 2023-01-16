e062a899cccac27b0a434f525851ab2a35978f47
belino93 <abelmadrid93@gmail.com>
2023-01-16 18:38:23 +0100

    Creamos estructura de express y conexión a BD mongoose

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+// Middlewares
+app.use(express.json());
+
+mongoose.connect(
+  "mongodb://mongo:VWySO2kT4frHTftPTyj5@containers-us-west-173.railway.app:5436"
+);
+
+const connection = mongoose.connection;
+
+connection.once("open", () => {
+  console.log("Conectado a la BD");
+});
+connection.on("error", (error) => {
+  console.log("Error en la conexión a la BD: ", error);
+});
+
+// Modelo
+const Contrato = mongoose.model("Contrato", {
+  nombre: { type: "String", required: true },
+  apellido1: "String",
+  apellido2: "String",
+  documento: { type: "String", required: true },
+  cp: { type: "Number", required: true },
+  localidad: "String",
+  direccion: "String",
+  telefono: { type: "String", required: true },
+});
+
+//Rutas
+app.get("/", (req, res) => {
+  res.json({
+    response: "success",
+  });
+});
+
+// Añade un contrato con los datos
+app.get("/addcontract", (req, res) => {
+  res.json({
+    response: "add contract success",
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Servidor en puerto 3000");
+});
