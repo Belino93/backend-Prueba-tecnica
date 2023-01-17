0e008978324bdee5c04818d2b6a390790046dea7
belino93 <abelmadrid93@gmail.com>
2023-01-17 10:13:23 +0100

    add locality route

--- routers/contratos.router.js
+++ routers/contratos.router.js
@@ -5,12 +5,14 @@ const {
     contratoCreateController,
     contratosGetAllController,
     contratoPatchController,
-    contratoPatchDeleteController
+    contratoPatchDeleteController,
+    getLocalidad
 } = require('../controllers/contratos.controllers')
 
 contratosRouter.post('/addcontract', contratoCreateController);
 contratosRouter.get('/listcontracts', contratosGetAllController)
 contratosRouter.patch('/modifycontract', contratoPatchController)
 contratosRouter.patch('/deletecontract', contratoPatchDeleteController)
+contratosRouter.get('/getlocalidad', getLocalidad)
 
 module.exports = contratosRouter;
