06f000a3e5ecef55ef240f2bf5de3429b8ac5ff9
belino93 <abelmadrid93@gmail.com>
2023-01-17 16:36:07 +0100

    update with cors options

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const mongoose = require("./config/mongoose.config.js");
-const contratosRouter = require("./routers/contratos.router")
+const contratosRouter = require("./routers/contratos.router");
 const app = express();
-let PORT = process.env.PORT ||3005
+let PORT = process.env.PORT || 3005;
+var corsOptions = {
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+};
 
 // Middlewares
 app.use(express.json());
@@ -16,10 +22,10 @@ connection.once("open", () => {
 connection.on("error", (error) => {
   console.log("Error en la conexión a la BD: ", error);
 });
-
+app.use(cors(corsOptions));
 //Rutas
-app.use("/", contratosRouter);
 
+app.use("/", contratosRouter);
 
 app.listen(PORT, () => {
   console.log("Servidor levantado en el puerto: " + PORT);
